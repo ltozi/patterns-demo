cf109ababe673dfc89a6d57466f242974e050c71
Federico <federico.baccaini@gmail.com>
2020-03-20 12:10:27 +0100

    aggiunta eccezione

--- src/main/java/com/kaleyra/patterns/singleton/federico/Singleton.java
+++ src/main/java/com/kaleyra/patterns/singleton/federico/Singleton.java
@@ -2,10 +2,18 @@ package com.kaleyra.patterns.singleton.federico;
 
 public class Singleton {
 
+    Singleton() throws InstantiationException {
+        if (instance != null) throw new InstantiationException();
+    }
+
     static Singleton instance = null;
 
     static Singleton getInstance() {
-        if (instance == null) instance = new Singleton();
+        if (instance == null) try {
+            instance = new Singleton();
+        } catch (InstantiationException e) {
+        }
+
         return instance;
     }
 }
