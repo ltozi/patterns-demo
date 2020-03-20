8f6f6065c77547c533cc8f4a33395293fd40e982
ltozi <luigi.toziani@kaleyra.com>
2020-03-20 12:21:58 +0100

    Added test structure

--- src/test/java/com/kaleyra/patterns/singleton/luigi/SingletonTest.java
+++ src/test/java/com/kaleyra/patterns/singleton/luigi/SingletonTest.java
@@ -6,6 +6,12 @@ public class SingletonTest {
 
     @Test
     public void shouldGetOneIstance() {
-        //TODO Singletong.getInstance().......
+        System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
+                "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
+                "RESULT:" + "\n");
+        Singleton singleton = Singleton.getInstance("FOO");
+        Singleton anotherSingleton = Singleton.getInstance("BAR");
+        System.out.println(singleton.value);
+        System.out.println(anotherSingleton.value);
     }
 }
